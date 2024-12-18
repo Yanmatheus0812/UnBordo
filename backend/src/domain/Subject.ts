09eb2d76392c37d0f9a583e3e37449b1fbb58ecf
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2024-12-18 18:35:59 -0300

    feat(domain): add subject interface

---
+++ backend/src/domain/Subject.ts
@@ -0,0 +1,6 @@
+export type Subject = {
+  id: string;
+  code: string;
+  name: string;
+  departmentCode: number;
+}
--- backend/src/domain/index.ts
+++ backend/src/domain/index.ts
@@ -1,2 +1,3 @@
 export * from './Student';
 export * from './Question';
+export * from './Subject';
