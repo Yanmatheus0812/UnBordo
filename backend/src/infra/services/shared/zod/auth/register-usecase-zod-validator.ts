2ea0f504dcb5480e5a668bb6a4d9721edd17c569
David William <sluuckejoohn@gmail.com>
2024-12-22 18:10:57 -0300

    feat: add register use case zod validator

--- backend/src/infra/services/shared/zod/auth/register-usecase-zod-validator.ts
+++ backend/src/infra/services/shared/zod/auth/register-usecase-zod-validator.ts
@@ -4,7 +4,6 @@ import { RegisterUsecase } from '@/application/usecases';
 import { Course } from '@/domain';
 import { z } from 'zod';
 
-
 export class RegisterUsecaseZodValidator implements Validator<RegisterUsecase.Input> {
   private readonly schema = z
     .object({
