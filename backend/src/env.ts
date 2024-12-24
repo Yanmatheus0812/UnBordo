dc17f7398285ae41d17c782d0bc4f91c46da1da1
gabriel <gabrielmagioli1@gmail.com>
2024-12-23 23:18:18 -0300

    fix(jwt-token): Fixed the implementation of token service

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -7,7 +7,10 @@ const envSchema = z.object({
   DATABASE_URL: z.string(),
   JWT_SECRET_KEY: z.string(),
   JWT_TOKEN_EXPIRATION: z.string(),
+<<<<<<< HEAD
   REDIS_URL: z.string(),
+=======
+>>>>>>> 8d643ff (fix(jwt-token): Fixed the implementation of token service)
 });
 
 const parsedEnv = envSchema.safeParse(process.env);
