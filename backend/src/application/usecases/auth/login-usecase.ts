689789cd4931763c989fcf7696c897e61321ceff
Sophiassilva <162454522+Sophiassilva@users.noreply.github.com>
2024-12-27 09:21:08 -0300

    refactor(login-usercase): updating login-usercase

--- backend/src/application/usecases/auth/login-usecase.ts
+++ backend/src/application/usecases/auth/login-usecase.ts
@@ -1,11 +1,34 @@
+import { StudentRepository } from '@/application/repositories/student-repository';
+import { Validator } from '@/application/services';
+import { StudentTokenManager } from '@/application/services/token/token-manager';
+import * as bcrypt from 'bcrypt';
+
 export class LoginUsecase {
   public static Name = 'LoginUsecase' as const;
 
-  constructor() {}
-  async execute(
-    _input: LoginUsecase.Input,
-  ): Promise<LoginUsecase.Output> {
-    throw new Error('Not implemented.');
+  constructor(
+    private readonly studentRepository: StudentRepository,
+    private readonly studentTokenManager: StudentTokenManager,
+    private readonly validator: Validator<LoginUsecase.Input>,
+  ) {}
+
+  async execute(_input: LoginUsecase.Input): Promise<LoginUsecase.Output> {
+    const validateInput = await this.validator.validate(_input);
+    const studentExists = await this.studentRepository.findBy({ registration: validateInput.registration });
+    if (!studentExists) {
+      throw new Error('Estudante não encontrado!');
+    }
+
+    const authenticatedStudent = await bcrypt.compare(_input.password, studentExists.password);
+    if (!authenticatedStudent) {
+      throw new Error('Senha incorreta!');
+      // Ajustar exceção
+    }
+    // Arrumar BIBILIOTECA!!!
+    // Injetar as dependências 
+    // implementar TOKEN!!
+    // Retornar o accessToken e o timestamp de expiração do campo;
+
   }
 }
 
