8cebc9ef1e79443c3ba6577959b27738b91694d7
Sophiassilva <162454522+Sophiassilva@users.noreply.github.com>
2024-12-27 09:21:08 -0300

    refactor(login-usercase): updating login-usercase

--- backend/src/application/usecases/auth/login-usecase.ts
+++ backend/src/application/usecases/auth/login-usecase.ts
@@ -1,7 +1,13 @@
 import { StudentRepository } from '@/application/repositories/student-repository';
+<<<<<<< HEAD
 import { StudentTokenManager, Validator } from '@/application/services';
 import { env } from '@/env';
 import { BcryptPasswordHash } from '@/infra/services/password';
+=======
+import { Validator } from '@/application/services';
+import { StudentTokenManager } from '@/application/services/token/token-manager';
+import * as bcrypt from 'bcrypt';
+>>>>>>> c3d3274 (refactor(login-usercase): updating login-usercase)
 
 export class LoginUsecase {
   public static Name = 'LoginUsecase' as const;
@@ -10,7 +16,10 @@ export class LoginUsecase {
     private readonly studentRepository: StudentRepository,
     private readonly studentTokenManager: StudentTokenManager,
     private readonly validator: Validator<LoginUsecase.Input>,
+<<<<<<< HEAD
     private readonly cryptographyService: BcryptPasswordHash,
+=======
+>>>>>>> c3d3274 (refactor(login-usercase): updating login-usercase)
   ) {}
 
   async execute(_input: LoginUsecase.Input): Promise<LoginUsecase.Output> {
@@ -19,12 +28,26 @@ export class LoginUsecase {
     if (!studentExists) {
       throw new Error('Estudante não encontrado!');
     }
+<<<<<<< HEAD
     const authenticatedStudent = await this.cryptographyService.compare(_input.password, studentExists.password);
     if (!authenticatedStudent) {
       throw new Error('Senha incorreta!');
     }
     const token = await this.studentTokenManager.generate({ studentId: validateInput.registration });
     return { accessToken: token, expiresAt: env.JWT_TOKEN_EXPIRATION };
+=======
+
+    const authenticatedStudent = await bcrypt.compare(_input.password, studentExists.password);
+    if (!authenticatedStudent) {
+      throw new Error('Senha incorreta!');
+      // Ajustar exceção
+    }
+    // Arrumar BIBILIOTECA!!!
+    // Injetar as dependências 
+    // implementar TOKEN!!
+    // Retornar o accessToken e o timestamp de expiração do campo;
+
+>>>>>>> c3d3274 (refactor(login-usercase): updating login-usercase)
   }
 }
 
