a55ca2b7326d3d7061ed29b11bca9a99f255c3bc
Sophia Silva <162454522+Sophiassilva@users.noreply.github.com>
2025-01-06 08:41:54 -0300

    refactor(login-usecase): updating login-usecase.ts

--- backend/src/application/usecases/auth/login-usecase.ts
+++ backend/src/application/usecases/auth/login-usecase.ts
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 import { UnauthenticatedError } from '@/application/error';
 import { NotFoundError } from '@/application/error/not-found-error';
 import { StudentRepository } from '@/application/repositories/student-repository';
 import { PasswordHash, StudentTokenManager, Validator } from '@/application/services';
 import { env } from '@/env';
-=======
-import { StudentRepository } from '@/application/repositories/student-repository';
-import { StudentTokenManager, Validator } from '@/application/services';
-import * as bcrypt from 'bcrypt';
->>>>>>> d8c72d7 (refactor(login-usercase): updating login-usercase)
 
 export class LoginUsecase {
   public static Name = 'LoginUsecase' as const;
@@ -17,17 +11,13 @@ export class LoginUsecase {
     private readonly studentRepository: StudentRepository,
     private readonly studentTokenManager: StudentTokenManager,
     private readonly validator: Validator<LoginUsecase.Input>,
-<<<<<<< HEAD
     private readonly cryptographyService: PasswordHash,
-=======
->>>>>>> d8c72d7 (refactor(login-usercase): updating login-usercase)
   ) {}
 
   async execute(_input: LoginUsecase.Input): Promise<LoginUsecase.Output> {
     const validateInput = await this.validator.validate(_input);
     const studentExists = await this.studentRepository.findBy({ registration: validateInput.registration });
     if (!studentExists) {
-<<<<<<< HEAD
       throw new NotFoundError('User not found', 'STUDENT');
     }
     const authenticatedStudent = await this.cryptographyService.compare(_input.password, studentExists.password);
@@ -36,20 +26,6 @@ export class LoginUsecase {
     }
     const token = await this.studentTokenManager.generate({ studentId: validateInput.registration });
     return { accessToken: token, expiresAt: env.JWT_TOKEN_EXPIRATION };
-=======
-      throw new Error('Estudante não encontrado!');
-    }
-
-    const authenticatedStudent = await bcrypt.compare(_input.password, studentExists.password);
-    if (!authenticatedStudent) {
-      throw new Error('Senha incorreta!');
-      // Ajustar exceção
-    }
-    // Arrumar BIBILIOTECA!!!
-    // Injetar as dependências
-    // implementar TOKEN!!
-    // Retornar o accessToken e o timestamp de expiração do campo;
->>>>>>> d8c72d7 (refactor(login-usercase): updating login-usercase)
   }
 }
 
