cfde915d97b568b0657d9ddee8d9a3dba3daa36a
gabriel <gabrielmagioli1@gmail.com>
2025-01-13 22:28:15 -0300

    refactor(forum): refactoring code to filter questions while using the get all questions usecase

--- backend/src/application/usecases/forum/get-all-questions-usecase.ts
+++ backend/src/application/usecases/forum/get-all-questions-usecase.ts
@@ -1,15 +1,23 @@
-import { Question } from '@/domain';
+import { Question, QuestionDifficulties, QuestionUrgencies } from '@/domain';
 
 export class GetAllQuestionsUsecase {
   public static Name = 'GetAllQuestionsUsecase' as const;
 
   constructor() {}
-  async execute(): Promise<GetAllQuestionsUsecase.Output> {
+  async execute(_input: GetAllQuestionsUsecase.Input): Promise<GetAllQuestionsUsecase.Output> {
     throw new Error('Not implemented.');
   }
 }
 
 export namespace GetAllQuestionsUsecase {
+  export type Input = {
+    filter: Partial<
+      {
+        urgency: QuestionUrgencies;
+        difficulty: QuestionDifficulties;
+      }
+    >;
+  };
 
   export type Output = {
     question: Question;
--- backend/src/application/usecases/forum/index.ts
+++ backend/src/application/usecases/forum/index.ts
@@ -1,6 +1,4 @@
 export * from './create-question-usecase';
 export * from './delete-question-usecase';
-export * from './filter-question-difficulty-usecase';
-export * from './filter-question-urgency-usecase';
 export * from './get-all-questions-usecase';
 export * from './get-question-usecase';
