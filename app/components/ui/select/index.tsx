ce25347cec0280aae50167d8831bbb827dc53e85
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2025-01-15 22:41:20 -0300

    app: add select component

---
+++ app/components/ui/select/index.tsx
@@ -0,0 +1,85 @@
+import { View, Text, Pressable, Modal, TextInput } from "react-native";
+import GestureRecognizer from 'react-native-swipe-gestures';
+import { Input } from "../input2";
+
+export function Select ({
+    label,
+    placeholder,
+    value,
+    modalVisible,
+    setModalVisible,
+    children
+}: {
+    label: string,
+    placeholder: string,
+    value?: string,
+    modalVisible: boolean,
+    setModalVisible: (value: boolean) => void,
+    children?: React.ReactNode
+}) {
+
+    return <View style={{
+        display: "flex",
+        flexDirection: "row",
+        rowGap: 2,
+    }}>
+        <Input
+            onPress={() => setModalVisible(true)}
+            label={label}
+            placeholder={placeholder}
+            value={value}
+        />
+        <GestureRecognizer
+                onSwipeDown={() => setModalVisible(false)}
+            >
+                <Modal
+                    visible={modalVisible}
+                    onRequestClose={() => setModalVisible(false)}
+                    animationType="slide"
+                    transparent
+                    style={{
+                        height: "100%",
+                        borderRadius: 0,
+                    }}
+                >
+                    <Pressable
+                        style={{
+                            flex: 1.75,
+                        }}
+                        onPress={() => setModalVisible(false)}
+                    ></Pressable>
+                    <View
+                        style={{
+                            flex: 1,
+                            backgroundColor: "#FFFFFF",
+                            borderTopLeftRadius: 20,
+                            borderTopRightRadius: 20,
+                        }}
+                    >
+                        <View
+                            style={{
+                                height: 20,
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            <View
+                                style={{
+                                    height: 3,
+                                    width: "50%",
+                                    backgroundColor: "#E1E1E6",
+                                    borderRadius: 2,
+                                }}
+                            >
+
+                            </View>
+                        </View>
+                        <View>
+                            {children}
+                        </View>
+                    </View>
+                </Modal>
+        </GestureRecognizer>
+    </View>
+}
