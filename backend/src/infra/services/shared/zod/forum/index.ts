f675108b1e96f620fdc08ab46ca8ca979fb34940
gabriel <gabrielmagioli1@gmail.com>
2025-01-17 17:56:29 -0300

    refactor(zod): updating imports from zod validators

---
+++ backend/src/infra/services/shared/zod/forum/index.ts
@@ -0,0 +1,3 @@
+export * from './forum-create-question-usecase-zod-validator';
+export * from './forum-delete-question-usecase-zod-validator';
+export * from './forum-get-question-usecase-zod-validator';
