4f18203f688c4ae49e1bfdbc399fef38b7d11c4c
gabriel <gabrielmagioli1@gmail.com>
2025-01-17 15:38:03 -0300

    feat(validator): Created create question validator

---
+++ backend/src/infra/services/shared/zod/forum/forum-create-question-usecase-zod-validator.ts
@@ -0,0 +1,27 @@
+import { ValidationError } from '@/application/error';
+import { Validator } from '@/application/services';
+import { CreateQuestionUsecase } from '@/application/usecases';
+import { QuestionDifficulty, QuestionUrgency } from '@/domain';
+import { z } from 'zod';
+
+export class ForumCreateQuestionUsecaseValidator implements Validator<CreateQuestionUsecase.Input> {
+  private readonly schema = z
+    .object({
+      subjectId: z.string(),
+      title: z.string(),
+      description: z.string(),
+      points: z.number().int(),
+      difficulty: z.nativeEnum(QuestionDifficulty),
+      urgency: z.nativeEnum(QuestionUrgency),
+    });
+
+  async validate(input: any): Promise<CreateQuestionUsecase.Input> {
+    const validateResult = await this.schema.safeParseAsync(input);
+
+    if (!(validateResult).success) {
+      throw new ValidationError(validateResult.error.issues);
+    }
+
+    return validateResult.data as CreateQuestionUsecase.Input;
+  }
+}
