bf318d2cd77737779bb6aa7c42854f21de48cdb3
gabriel <gabrielmagioli1@gmail.com>
2025-01-17 18:03:37 -0300

    feat(validator): Implemented get all questions zod validator

---
+++ backend/src/infra/services/shared/zod/forum/forum-get-all-questions-usecase-zod-validator.ts
@@ -0,0 +1,22 @@
+import { ValidationError } from '@/application/error';
+import { Validator } from '@/application/services';
+import { GetAllQuestionsUsecase, GetQuestionUsecase } from '@/application/usecases';
+import { QuestionDifficulty, QuestionUrgency } from '@/domain';
+import { z } from 'zod';
+
+export class ForumGetAllQuestionsUsecaseZodValidator implements Validator<GetAllQuestionsUsecase.Input> {
+  private readonly schema = z
+    .object({
+      difficulty: z.nativeEnum(QuestionDifficulty).optional(),
+      urgency: z.nativeEnum(QuestionUrgency).optional(),
+    });
+
+  async validate(input: any): Promise<GetAllQuestionsUsecase.Input> {
+    const validateResult = await this.schema.safeParseAsync(input);
+
+    if (!(validateResult).success) {
+      throw new ValidationError(validateResult.error.issues);
+    }
+    return validateResult.data as GetAllQuestionsUsecase.Input;
+  }
+}
