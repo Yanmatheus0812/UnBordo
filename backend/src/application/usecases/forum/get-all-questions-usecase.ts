1720822e7bc6f4d0a755a7b3521ca96de9a95273
gabriel <gabrielmagioli1@gmail.com>
2025-01-19 12:05:14 -0300

    feat(forum): Implemented the get all questions usecase

--- backend/src/application/usecases/forum/get-all-questions-usecase.ts
+++ backend/src/application/usecases/forum/get-all-questions-usecase.ts
@@ -1,11 +1,25 @@
+import { NotFoundError } from '@/application/error';
+import { QuestionRepository } from '@/application/repositories';
+import { Validator } from '@/application/services';
 import { Question, QuestionDifficulties, QuestionUrgencies } from '@/domain';
 
 export class GetAllQuestionsUsecase {
   public static Name = 'GetAllQuestionsUsecase' as const;
 
-  constructor() {}
-  async execute(_input: GetAllQuestionsUsecase.Input): Promise<GetAllQuestionsUsecase.Output> {
-    throw new Error('Not implemented.');
+  constructor(
+    private readonly questionRepository: QuestionRepository,
+    private readonly validator: Validator<GetAllQuestionsUsecase.Input>,
+  ) {}
+
+  async execute(input: GetAllQuestionsUsecase.Input): Promise<GetAllQuestionsUsecase.Output> {
+    const validatedInput = await this.validator.validate(input);
+
+    const questions = await this.questionRepository.findAll({ urgency: validatedInput.filter.urgency, difficulty: validatedInput.filter.difficulty });
+    if (questions.length === 0) {
+      throw new NotFoundError('Questions not found', 'QUESTION');
+    }
+
+    return { questions };
   }
 }
 
