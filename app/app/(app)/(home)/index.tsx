7938aaa6b0c2cf01f1b05ccbd575cf466d486e0b
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2025-01-19 01:40:02 -0300

    feat(app): finish homescreen
    Co-authored-by: leticialopes20 <leticialopes20@users.noreply.github.com>

--- app/app/(app)/(home)/index.tsx
+++ app/app/(app)/(home)/index.tsx
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, StyleSheet, FlatList, TouchableOpacity, Modal } from "react-native";
+import { View, Text, TextInput, StyleSheet, FlatList, TouchableOpacity, Modal, Pressable } from "react-native";
 import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
 import UnbLogo_Home from "@/assets/images/UnbLogo_Home";
 import { Input } from "@/components/ui/input";
 import Filter from "@/assets/images/Filter";
 import SearchIcon from "@/assets/images/SearchIcon";
+import Balloon from "@/assets/images/Balloon";
+import HelmIcon from "@/assets/images/HelmIcon";
+import WoodButton from "@/assets/images/WoodButton";
+import { useRouter } from "expo-router";
 
 const ForumHome = () => {
   const Forumdata = [
     {
       id: 1,
       registration: "251012345",
-      subject: "Introdução à Algebra Linear",
+      subject: "Tópicos Especiais em Matemática II",
       text: "Odeiooo ESPAÇO VETORIALLL",
       urgency: false,
       difficulty: "Média",
@@ -44,7 +48,7 @@ const ForumHome = () => {
       id: 5,
       registration: "251010553",
       subject: "Felicidade",
-      text: "Como pode a máteria se chamar Felicidade e eu só ficar triste quando assisto à ela?",
+      text: "Como pode a máteria se chamar Felicidade e eu só ficar triste quando assisto à ela? Como pode a máteria se chamar Felicidade e eu só ficar triste quando assisto à ela? Como pode a máteria se chamar Felicidade e eu só ficar triste quando assisto à ela? Como pode a máteria se chamar Felicidade e eu só ficar triste quando assisto à ela? Como pode a máteria se chamar Felicidade e eu só ficar triste quando assisto à ela? Como pode a máteria se chamar Felicidade e eu só ficar triste quando assisto à ela?",
       urgency: true,
       difficulty: "Alta",
     },
@@ -76,37 +80,63 @@ const ForumHome = () => {
     return (
       <View style={styles.card}>
         <View style={styles.cardHeader}>
+
           <FontAwesome name="user-circle" size={40} color="#173CAC" />
-          <View style={{ marginLeft: 10 }}>
+          <View style={{
+            marginLeft: 10,
+            //backgroundColor: "red",
+            maxWidth: "100%",
+          
+          }}>
             <Text style={styles.registration}>{item.registration}</Text>
             <Text style={styles.subject}>{item.subject}</Text>
           </View>
-          {item.urgency && (
-            <View style={styles.urgentTag}>
-              <Text style={styles.urgentText}>Urgente</Text>
-            </View>
-          )}
-          <TouchableOpacity onPress={toggleModal}>
-            <MaterialIcons
-              name="more-vert"
-              size={24}
-              color="#000"
-              style={styles.icon}
-            />
-          </TouchableOpacity>
+         
+          <View
+            style={{
+              marginLeft: "auto",
+              flexDirection: "row",
+              alignItems: "center",
+              columnGap: 15
+            }}
+          >
+            {item.urgency && (
+              <View style={styles.urgentTag}>
+                <Text style={styles.urgentText}>Urgente</Text>
+              </View>
+            )}
+             <Balloon/>
+
+            <TouchableOpacity
+              onPress={toggleModal}
+            >
+              <MaterialIcons
+                name="more-vert"
+                size={24}
+                color="#000"
+                style={styles.icon}
+              />
+            </TouchableOpacity>
+          </View>
+
         </View>
         <Text style={styles.text} numberOfLines={isExpanded ? undefined : 3}>
           {item.text}
         </Text>
-        {!isExpanded ? (
-          <TouchableOpacity onPress={() => handleReadMore(item.id)}>
-            <Text style={styles.readMore}>Ler mais...</Text>
-          </TouchableOpacity>
-        ) : (
-          <TouchableOpacity onPress={handleShowLess}>
-            <Text style={styles.readMore}>Mostrar menos</Text>
-          </TouchableOpacity>
-        )}
+        {
+          (item.text.length > 100 || item.text.split("\n").length > 3)
+          && (
+            !isExpanded ? (
+              <TouchableOpacity onPress={() => handleReadMore(item.id)}>
+                <Text style={styles.readMore}>Ler mais...</Text>
+              </TouchableOpacity>
+            ) : (
+              <TouchableOpacity onPress={handleShowLess}>
+                <Text style={styles.readMore}>Mostrar menos</Text>
+              </TouchableOpacity>
+            )
+          )
+        }
         <Text style={styles.difficulty}>Dificuldade: {item.difficulty}</Text>
       </View>
     );
@@ -121,18 +151,34 @@ const ForumHome = () => {
     difficulty: string;
   };
 
+  const router = useRouter();
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <UnbLogo_Home style={styles.title}></UnbLogo_Home>
+        <UnbLogo_Home style={styles.title}/>
+        <HelmIcon style={{
+            position: "absolute",
+            right: 0,
+        }}/>
       </View>
-
-      <View style={styles.searchBar}>
-        <Input>
+      
+      <View style={{
+        flexDirection: "row",
+        width: "100%",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingHorizontal: "5%",
+      }}>
+        <Input style={styles.searchBar}>
+          <SearchIcon/>
           <TextInput placeholder="Pesquisar" style={styles.searchInput} />
-          <SearchIcon></SearchIcon>
         </Input>
-        <Filter></Filter>
+        <Pressable
+          onPress={() => router.push("/(app)/(home)/(filter)")}
+        >
+          <Filter/>
+        </Pressable>
       </View>
 
       <FlatList
@@ -142,8 +188,30 @@ const ForumHome = () => {
         contentContainerStyle={{ paddingHorizontal: 20 }}
       />
 
-      <TouchableOpacity style={styles.fab}>
-        <MaterialIcons name="add" size={30} color="#fff" />
+      <TouchableOpacity
+        style={{
+          position: "absolute",
+          bottom: "2.5%",
+          right: "2.5%",
+          elevation: 16,
+          shadowOffset: { width: -3, height: 3 },
+          shadowOpacity: 0.1,
+          shadowRadius: 1,
+          borderRadius: "100%",
+          width: 70,
+          height: 70
+        }}
+      >
+        <WoodButton
+          style={{
+            shadowColor: '#000000',
+            shadowOffset: { width: -10, height: 10 },
+            shadowOpacity: 0.75,
+            shadowRadius: 3,
+            borderRadius: "100%"
+          }}
+
+        />
       </TouchableOpacity>
 
       <Modal
@@ -168,23 +236,31 @@ const ForumHome = () => {
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: "#F5F6FA" },
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F6FA",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   header: { 
     backgroundColor: "#173CAC", 
     padding: 20, 
     height: 103,
-    width: 375,
+    width: "100%",
+    justifyContent: "flex-end"
 },
   title: { color: "#FFFFFF", fontSize: 24, fontWeight: "bold", paddingLeft: 20 },
   searchBar: {
     flexDirection: "row",
     alignItems: "center",
     backgroundColor: "#fff",
-    margin: 15,
+    marginVertical: 15,
     height: 46,
-    width: 289,
+    width: "90%",
     paddingHorizontal: 10,
-    borderRadius: 8,
+    borderRadius: 16,
+    borderWidth: 1,
+    borderColor: "#969696",
     elevation: 3,
   },
   searchInput: { 
@@ -201,9 +277,17 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     elevation: 2,
   },
-  cardHeader: { flexDirection: "row", alignItems: "center" },
+  cardHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+    position: "relative"
+    //backgroundColor: "red"
+},
   registration: { fontSize: 16, fontWeight: "bold", fontFamily: 'Itim_400Regular' },
-  subject: { fontSize: 14, color: "#666", fontFamily: 'Itim_400Regular'},
+  subject: { fontSize: 14, 
+    color: "#666", 
+    fontFamily: 'Itim_400Regular',
+  },
   urgentTag: {
     backgroundColor: "#FF0000",
     borderRadius: 16,
@@ -228,7 +312,7 @@ const styles = StyleSheet.create({
     elevation: 5,
   },
   icon: {
-    marginLeft: 45,
+    marginLeft: "auto",
   },
   modalBackground: {
     flex: 1,
