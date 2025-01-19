fac81c8fb7ee5b597518905f9d48ec35ce864839
gabriel <gabrielmagioli1@gmail.com>
2025-01-19 12:01:10 -0300

    refactor(repository): Created the findAll method in question repository

--- backend/src/application/repositories/question-repository.ts
+++ backend/src/application/repositories/question-repository.ts
@@ -27,6 +27,15 @@ export namespace QuestionRepository {
     export type Output = Question;
   };
 
+  export namespace FindAll {
+    export type Input = Partial<{
+      urgency: QuestionUrgencies;
+      difficulty: QuestionDifficulties;
+    }>;
+
+    export type Output = Question[];
+  };
+
   export namespace Delete {
     export type Input = {
       id: string;
@@ -40,5 +49,6 @@ export interface QuestionRepository {
   create: (params: QuestionRepository.Create.Input) => Promise<QuestionRepository.Create.Output>;
   update: (id: string, params: QuestionRepository.Update.Input) => Promise<QuestionRepository.Update.Output>;
   findBy: (params: QuestionRepository.FindBy.Input) => Promise<QuestionRepository.FindBy.Output>;
+  findAll: (params: QuestionRepository.FindAll.Input) => Promise<QuestionRepository.FindAll.Output>;
   delete: (params: QuestionRepository.Delete.Input) => Promise<QuestionRepository.Delete.Output>;
 };
