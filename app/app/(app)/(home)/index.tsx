f9842ed7e6fca65eb1ed64085987917a18d1f273
@letilopes20 <letilopes2004@gmail.com>
2025-01-19 00:01:15 -0300

    Corrigido arquivo index.tsx

--- app/app/(app)/(home)/index.tsx
+++ app/app/(app)/(home)/index.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, StyleSheet, FlatList, TouchableOpacity, Modal } from "react-native";
-// import forumBox from '@/components/ui/forum/forumBox'
 import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
 import UnbLogo_Home from "@/assets/images/UnbLogo_Home";
 import { Input } from "@/components/ui/input";
 import Filter from "@/assets/images/Filter";
+import SearchIcon from "@/assets/images/SearchIcon";
 
-const ForumHome = () =>{
+const ForumHome = () => {
   const Forumdata = [
     {
       id: 1,
@@ -30,99 +30,111 @@ const ForumHome = () =>{
       subject: "Cálculo 2",
       text: "ALGUÉM ME AJUDA COM ESSE SOMATÓRIOOO, POR FAVORRRR",
       urgency: true,
-      difficulty: "Alta"
-  
+      difficulty: "Alta",
     },
-
     {
       id: 4,
       registration: "252010333",
       subject: "Felicidade",
       text: "Como pode a máteria se chamar Felicidade e eu só ficar triste quando assisto à ela?",
       urgency: true,
-      difficulty: "Alta"
-  
+      difficulty: "Alta",
     },
-
     {
       id: 5,
       registration: "251010553",
       subject: "Felicidade",
       text: "Como pode a máteria se chamar Felicidade e eu só ficar triste quando assisto à ela?",
       urgency: true,
-      difficulty: "Alta"
-  
-    }
-
+      difficulty: "Alta",
+    },
   ];
 
   const [modalVisible, setModalVisible] = useState(false);
+  const [expandedText, setExpandedText] = useState<number | null>(null); // Armazena o ID do item expandido
+
   const toggleModal = () => {
     setModalVisible(!modalVisible);
   };
+
   const handleOptionClick = (option: string) => {
     console.log(`Opção selecionada: ${option}`);
-    setModalVisible(false);  // Fecha a modal após selecionar uma opção
+    setModalVisible(false); 
   };
 
-  const renderCard = ({ item}: { item: CardItem }) => (
-    <View style={styles.card}>
-      <View style={styles.cardHeader}>
-        <FontAwesome name="user-circle" size={40} color="#173CAC" />
-        <View style={{ marginLeft: 10 }}>
-          <Text style={styles.name}>{item.registration}</Text>
-          <Text style={styles.subject}>{item.subject}</Text>
-        </View>
-        {item.urgency && (
-          <View style={styles.urgentTag}>
-            <Text style={styles.urgentText}>Urgente</Text>
+  const handleReadMore = (id: number) => {
+    setExpandedText(id);
+  };
+
+  const handleShowLess = () => {
+    setExpandedText(null);
+  };
+
+  const renderCard = ({ item }: { item: CardItem }) => {
+    const isExpanded = expandedText === item.id;
+
+    return (
+      <View style={styles.card}>
+        <View style={styles.cardHeader}>
+          <FontAwesome name="user-circle" size={40} color="#173CAC" />
+          <View style={{ marginLeft: 10 }}>
+            <Text style={styles.registration}>{item.registration}</Text>
+            <Text style={styles.subject}>{item.subject}</Text>
           </View>
-
-
-
-          
+          {item.urgency && (
+            <View style={styles.urgentTag}>
+              <Text style={styles.urgentText}>Urgente</Text>
+            </View>
+          )}
+          <TouchableOpacity onPress={toggleModal}>
+            <MaterialIcons
+              name="more-vert"
+              size={24}
+              color="#000"
+              style={styles.icon}
+            />
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.text} numberOfLines={isExpanded ? undefined : 3}>
+          {item.text}
+        </Text>
+        {!isExpanded ? (
+          <TouchableOpacity onPress={() => handleReadMore(item.id)}>
+            <Text style={styles.readMore}>Ler mais...</Text>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity onPress={handleShowLess}>
+            <Text style={styles.readMore}>Mostrar menos</Text>
+          </TouchableOpacity>
         )}
-        {/* Ícone clicável */}
-      <TouchableOpacity onPress={toggleModal}>
-        <MaterialIcons
-          name="more-vert"
-          size={24}
-          color="#000"
-          style={styles.icon}
-        />
-      </TouchableOpacity>
+        <Text style={styles.difficulty}>Dificuldade: {item.difficulty}</Text>
       </View>
-      <Text style={styles.text}>{item.text}</Text>
-      <TouchableOpacity>
-        <Text style={styles.readMore}>Ler mais...</Text>
-      </TouchableOpacity>
-      <Text style={styles.difficulty}>Dificuldade: {item.difficulty}</Text>
-    </View>
-  );
+    );
+  };
+
   type CardItem = {
+    id: number;
     registration: string;
     subject: string;
-    urgency?: boolean; // `urgency` é opcional
+    urgency?: boolean;
     text: string;
     difficulty: string;
   };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <UnbLogo_Home></UnbLogo_Home>
+        <UnbLogo_Home style={styles.title}></UnbLogo_Home>
       </View>
 
       <View style={styles.searchBar}>
         <Input>
-        <TextInput placeholder="Pesquisar" style={styles.searchInput} />
+          <TextInput placeholder="Pesquisar" style={styles.searchInput} />
+          <SearchIcon></SearchIcon>
         </Input>
-      </View>
-
-      <View>
         <Filter></Filter>
       </View>
 
-      {/* Cards */}
       <FlatList
         data={Forumdata}
         keyExtractor={(item) => item.id.toString()}
@@ -130,23 +142,24 @@ const ForumHome = () =>{
         contentContainerStyle={{ paddingHorizontal: 20 }}
       />
 
-      {/* Floating Action Button */}
       <TouchableOpacity style={styles.fab}>
         <MaterialIcons name="add" size={30} color="#fff" />
       </TouchableOpacity>
 
-          {/* Modal */}
       <Modal
         visible={modalVisible}
         transparent={true}
         animationType="fade"
-        onRequestClose={toggleModal}  // Permite fechar a modal ao pressionar o botão de "Voltar"
+        onRequestClose={toggleModal}
       >
         <View style={styles.modalBackground}>
           <View style={styles.modalContent}>
-            <Text style={styles.modalOption} onPress={() => handleOptionClick('Opção 1')}>Responder</Text>
-            <Text style={styles.modalOption} onPress={() => handleOptionClick('Opção 2')}>Denunciar</Text>
-
+            <Text style={styles.modalOption} onPress={() => handleOptionClick("Opção 1")}>
+              Responder
+            </Text>
+            <Text style={styles.modalOption} onPress={() => handleOptionClick("Opção 2")}>
+              Denunciar
+            </Text>
           </View>
         </View>
       </Modal>
@@ -179,6 +192,7 @@ const styles = StyleSheet.create({
     marginLeft: 10, 
     fontSize: 16, 
     color: "969696", 
+    fontFamily: 'Itim_400Regular'
   },
   card: {
     backgroundColor: "#fff",
@@ -188,8 +202,8 @@ const styles = StyleSheet.create({
     elevation: 2,
   },
   cardHeader: { flexDirection: "row", alignItems: "center" },
-  name: { fontSize: 16, fontWeight: "bold" },
-  subject: { fontSize: 14, color: "#666" },
+  registration: { fontSize: 16, fontWeight: "bold", fontFamily: 'Itim_400Regular' },
+  subject: { fontSize: 14, color: "#666", fontFamily: 'Itim_400Regular'},
   urgentTag: {
     backgroundColor: "#FF0000",
     borderRadius: 16,
@@ -197,10 +211,10 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
     marginLeft: 16,
   },
-  urgentText: { color: "#fff", fontSize: 13 },
-  text: { fontSize: 14, color: "#333", marginVertical: 10 },
-  readMore: { fontSize: 13, color: "#173CAC", fontWeight: "bold", textDecorationLine: 'underline' },
-  difficulty: { fontSize: 12, color: "0000", marginTop: 5, alignSelf: "flex-end" },
+  urgentText: { color: "#fff", fontSize: 13,  fontFamily: 'Itim_400Regular'},
+  text: { fontSize: 14, color: "#333", marginVertical: 10, fontFamily: "Raleway_400Regular" },
+  readMore: { fontSize: 13, color: "#173CAC", fontWeight: "bold",fontFamily: 'Itim_400Regular', textDecorationLine: 'underline' },
+  difficulty: { fontSize: 12, color: "0000", marginTop: 5, fontFamily: 'Itim_400Regular', alignSelf: "flex-end" },
   fab: {
     position: "absolute",
     bottom: 20,
