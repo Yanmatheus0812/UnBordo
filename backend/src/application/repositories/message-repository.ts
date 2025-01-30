1f139f8f370e6e3bbc34e5bf374721cacce38f4b
gabriel <gabrielmagioli1@gmail.com>
2025-01-30 19:18:11 -0300

    feat(repository): Created the message repository

---
+++ backend/src/application/repositories/message-repository.ts
@@ -0,0 +1,34 @@
+export namespace MessageRepository {
+  export const Name = 'MessageRepository';
+
+  export namespace Create {
+    export type Input = Message;
+
+    export type Output = Message;
+  }
+
+  export namespace Delete {
+    export type Input = {
+      id: string;
+      message: Message;
+    };
+
+    export type Output = Message;
+  }
+
+  export namespace FindAllBy {
+    export type Input = Partial<{
+      quesitonId: string;
+      authorId: string;
+      chatRoomId: string;
+    }>;
+
+    export type Output = Array<Message>;
+  }
+}
+
+export interface QuestionRepository {
+  create: (params: MessageRepository.Create.Input,) => Promise<MessageRepository.Create.Output>;
+  delete: (params: MessageRepository.Delete.Input,) => Promise<MessageRepository.Delete.Output>;
+  findAllBy: (params: MessageRepository.FindAllBy.Input,) => Promise<MessageRepository.FindAllBy.Output>;
+}
