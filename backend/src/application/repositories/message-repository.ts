de8bdaf2cb45dc6808cdf47ce35e977e4c26490a
gabemagioli <gabrielmagioli1@gmail.com>
2025-01-31 11:20:39 -0300

    fix(repository): removed findAllBy method from message repository

--- backend/src/application/repositories/message-repository.ts
+++ backend/src/application/repositories/message-repository.ts
@@ -18,19 +18,9 @@ export namespace MessageRepository {
     export type Output = Message;
   }
 
-  export namespace FindAllBy {
-    export type Input = Partial<{
-      quesitonId: string;
-      authorId: string;
-      chatRoomId: string;
-    }>;
-
-    export type Output = Array<Message>;
-  }
 }
 
 export interface MessageRepository {
   create: (params: MessageRepository.Create.Input,) => Promise<MessageRepository.Create.Output>;
   delete: (params: MessageRepository.Delete.Input,) => Promise<MessageRepository.Delete.Output>;
-  findAllBy: (params: MessageRepository.FindAllBy.Input,) => Promise<MessageRepository.FindAllBy.Output>;
 }
