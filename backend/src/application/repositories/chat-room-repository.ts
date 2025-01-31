02cbb89480c28043c22a8ce632ae31d9be55ed4f
Sophiassilva <162454522+Sophiassilva@users.noreply.github.com>
2025-01-31 10:30:50 -0300

    add: create chat room repository

---
+++ backend/src/application/repositories/chat-room-repository.ts
@@ -0,0 +1,30 @@
+import { ChatRoom } from '@/domain';
+
+export namespace ChatRoomRepository {
+  export const Name = 'ChatRoomRepository';
+
+  export namespace Create {
+    export type Input = ChatRoom;
+
+    export type Output = ChatRoom;
+  };
+
+  export namespace GetMessages{
+    export type Input = { chatRoomId: string, };
+
+    export type Output = Pick<ChatRoom, 'messages'>;
+  }
+
+  export namespace Update{
+    export type Input = { status: 'OPEN' | 'CLOSED', };
+
+    export type Output = ChatRoom;
+  }
+
+}
+
+export interface ChatRoomRepository {
+  create: (params: ChatRoomRepository.Create.Input) => Promise<ChatRoomRepository.Create.Output>;
+  getMessages: (params: ChatRoomRepository.GetMessages.Input) => Promise<ChatRoomRepository.GetMessages.Output>;
+  update: (params: ChatRoomRepository.Update.Input) => Promise<ChatRoomRepository.Update.Output>;
+};
