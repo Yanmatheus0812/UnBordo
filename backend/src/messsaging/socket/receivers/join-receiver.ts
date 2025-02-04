8f464f226c025df84ecdb8bd133ca83138b1e97e
gabemagioli <gabrielmagioli1@gmail.com>
2025-02-03 22:35:53 -0300

    fix: fixed lint

--- backend/src/messsaging/socket/receivers/join-receiver.ts
+++ backend/src/messsaging/socket/receivers/join-receiver.ts
@@ -1,43 +1,45 @@
+import { StudentRepository } from '@/application/repositories';
 import { container } from '@/infra/container';
 import { Socket as IoSocket } from 'socket.io';
 import { Socket } from '..';
-import { StudentRepository } from '@/application/repositories';
 
-export const joinReceiver = (socket: IoSocket) => async () => {
-  const studentTokenManager = container.get('StudentTokenManager');
+export function joinReceiver(socket: IoSocket) {
+  return async () => {
+    const studentTokenManager = container.get('StudentTokenManager');
 
-  const tokenIsValid = await studentTokenManager.verify(
-    socket.handshake.auth.token,
-  );
+    const tokenIsValid = await studentTokenManager.verify(
+      socket.handshake.auth.token,
+    );
 
-  if (!tokenIsValid) {
-    socket.disconnect();
-    return;
-  }
+    if (!tokenIsValid) {
+      socket.disconnect();
+      return;
+    }
 
-  const tokenResult = await studentTokenManager.decrypt(
-    socket.handshake.auth.token,
-  );
+    const tokenResult = await studentTokenManager.decrypt(
+      socket.handshake.auth.token,
+    );
 
-  const repo = container.get(StudentRepository.Name);
+    const repo = container.get(StudentRepository.Name);
 
-  const student = await repo.findBy({
-    where: {
-      id: tokenResult.studentId,
-    },
-  });
+    const student = await repo.findBy({
+      where: {
+        id: tokenResult.studentId,
+      },
+    });
 
-  if (!student) {
-    socket.disconnect();
-    return;
-  }
+    if (!student) {
+      socket.disconnect();
+      return;
+    }
 
-  Socket.addNewStudent(
-    {
-      id: student.id,
-      name: student.name,
-      registration: student.registration,
-    },
-    socket,
-  );
-};
+    Socket.addNewStudent(
+      {
+        id: student.id,
+        name: student.name,
+        registration: student.registration,
+      },
+      socket,
+    );
+  };
+}
