f93fcd4c7cd01a18f8ec53d02e40bdedf4a60b69
@letilopes20 <letilopes2004@gmail.com>
2025-02-08 20:10:16 -0300

    feat: create Star components

---
+++ app/assets/images/StarWhite.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import Svg, { Path, SvgProps } from "react-native-svg";
+
+interface StarProps extends SvgProps {
+  size?: number;
+}
+
+const StarWhite: React.FC<StarProps> = ({ size = 32, ...props }) => {
+  return (
+    <Svg
+      width={size}
+      height={size} // 🟢 Agora o tamanho é dinâmico
+      viewBox="0 0 32 30"
+      fill="none"
+      {...props}
+    >
+      <Path
+        d="M16.476.845a.5.5 0 00-.951 0l-3.256 10.02H1.734a.5.5 0 00-.294.904l8.523 6.193L6.708 27.98a.5.5 0 00.77.559L16 22.348l8.523 6.192a.5.5 0 00.77-.56l-3.256-10.018 8.523-6.193a.5.5 0 00-.294-.904H19.73L16.475.845z"
+        fill="#D9D9D9"
+        stroke="#474747"
+        strokeLinejoin="round"
+      />
+    </Svg>
+  );
+};
+
+export default StarWhite;
