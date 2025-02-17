072a2beb19f56d0e080011585f8bb3589cf2446e
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2025-02-17 01:08:38 -0300

    feat(backend): add server notification set

--- backend/src/api/routes/v1/student/auth/index.ts
+++ backend/src/api/routes/v1/student/auth/index.ts
@@ -3,6 +3,7 @@ import loginRoute from './login-route';
 import registerConfirmRoute from './register-confirm-route';
 import registerRoute from './register-route';
 import forgotPasswordRoutes from './forgot-password';
+import sendToken from './send-token';
 
 const route = Router({ mergeParams: true });
 
@@ -10,5 +11,6 @@ route.post('/login', loginRoute);
 route.post('/register', registerRoute);
 route.get('/register/confirm/:code', registerConfirmRoute);
 route.use('/forgot-password', forgotPasswordRoutes);
+route.post('/send-token', sendToken);
 
 export default route;
---
+++ backend/src/api/routes/v1/student/auth/send-token.ts
@@ -0,0 +1,11 @@
+import { LoginUsecase } from '@/application/usecases';
+import { container } from '@/infra/container';
+import { Request, Response } from 'express';
+import { notification_devices } from '@/server';
+
+export default async function (req: Request, res: Response) {
+    console.log("token added: ", req.body?.token);
+    notification_devices.push(req.body?.token);
+    console.log("all tokens: ", notification_devices);
+    res.json({ message: "Token added" });
+}
